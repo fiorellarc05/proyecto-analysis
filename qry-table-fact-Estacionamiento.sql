5150ce15be636bc6a712fae90d0102e664231daa
AntonioGut <antonio.gutierrez002@gmail.com>
2021-03-17 03:34:37 +0000

    segunda parte

--- qry-table-fact-Estacionamiento.sql
+++ qry-table-fact-Estacionamiento.sql
@@ -1,11 +1,45 @@
-use CuboParqueo   if (OBJECT_ID ('dbo.FactEstacionamiento', 'U') > 0) drop table FactEstacionamiento;
+use CuboParqueo
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FactEstacionamiento]') AND type in (N'U'))
+DROP TABLE [dbo].[FactEstacionamiento]
+GO
+
+SELECT TarifaBase, Ganancia, Mantenimiento, ImpVentas, TotalACobrar, 
+DATEPART(HOUR, FechaHoraIngreso) as HoraEntrada, DATEPART(DAY, FechaHoraIngreso) as DiaEntrada,
+DATEPART(HOUR, FechaHoraSalida) as HoraSalida, DATEPART(DAY, FechaHoraSalida) as DiaSalida, 
+DATEDIFF(mi,FechaHoraSalida,FechaHoraIngreso) as CantidadMinutos,
+HoraEntrada as EstratoHoraEntrada,
+HoraSalida as EstratoHoraSalida,
+DiaEntrada as EstratoDiaEntrada,
+DiaSalida as EstratoDiaSalida,
+Ganancia as EstratoGancia
+into FactEstacionamiento FROM  ExamenAnalisis.dbo.Estacionamiento ;
+ 
+
+begin tran 
+update FactEstacionamiento set EstratoGancia = ( select e.Descripcion from ParqueoEstratos e 
+where Ganancia >= e.LimiteInferior and Ganancia < e.LimiteSuperior and e.TipoEstrato = 'Ganacia' );   
+commit;
+
+begin tran 
+update FactEstacionamiento set EstratoHoraEntrada = ( select e.Descripcion from ParqueoEstratos e 
+where HoraEntrada	>= e.LimiteInferior and HoraEntrada < e.LimiteSuperior and e.TipoEstrato = 'HoraEntrada' );   
+commit;
+
+begin tran 
+update FactEstacionamiento set EstratoHoraSalida = ( select e.Descripcion from ParqueoEstratos e 
+where HoraSalida	>= e.LimiteInferior and HoraSalida < e.LimiteSuperior and e.TipoEstrato = 'HoraSalida' );   
+commit;
+
+begin tran 
+update FactEstacionamiento set EstratoDiaEntrada = ( select e.Descripcion from ParqueoEstratos e 
+where DiaEntrada	>= e.LimiteInferior and DiaEntrada < e.LimiteSuperior and e.TipoEstrato = 'DiaRestriccion' );   
+commit;
+
+begin tran 
+update FactEstacionamiento set EstratoDiaSalida = ( select e.Descripcion from ParqueoEstratos e 
+where DiaSalida	>= e.LimiteInferior and DiaSalida < e.LimiteSuperior and e.TipoEstrato = 'DiaRestriccion' );   
+commit;
 
 
-SELECT CustomerID, EmployeeID, OrderDate, RequiredDate, ShippedDate, Freight, ShipName, ShipCity,
-isnull(ShipRegion,'N/A') as ShipRegion, ShipCountry, convert (varchar(50), ' ') EstratoFlete 
-into FactEstacionamiento FROM ExamenAnalisis.dbo.Estacionamiento ;
 
-
-begin tran update FactEstacionamiento set EstratoHoraEntrada = ( select e.Descripcion from ParqueoEstratos e 
-where HoraEntrada >= e.LimiteInferior and HoraEntrada < e.LimiteSuperior and e.TipoEstrato = 'HoraEntrada' );   commit;
 
