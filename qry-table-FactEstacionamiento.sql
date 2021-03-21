1f563c792cfe3cbdcbabfa92e2bd075ec049cbcf
Fiorella Rodríguez <fiorellarodriguez05.fr@gmail.com>
2021-03-21 22:43:26 +0000

    cambios fact

--- qry-table-FactEstacionamiento.sql
+++ qry-table-FactEstacionamiento.sql
@@ -4,25 +4,25 @@ if
 GO
 
 SELECT 
-TarifaBase, Mantenimiento, ImpVentas, /*RIGHT(v.Placa, 1) AS UltimoDigitoPlaca,*/ FechaHoraIngreso,
-DATEPART(HOUR, FechaHoraIngreso) as HoraEntrada, CONVERT(varchar(50), FechaHoraIngreso) as EstratoHoraEntrada,
-CONVERT(varchar(10), DATEPART(HOUR, FechaHoraIngreso)) as IndicativoHoraEntrada,
-DATEPART(DAY,FechaHoraIngreso) as DiaEntrada, 
-DATEPART(WEEKDAY,FechaHoraIngreso) as DiaSemanaEntrada,
-CONVERT(varchar(50),DATEPART(WEEKDAY,FechaHoraIngreso)) as EntradaRestriccion,
-CONVERT(varchar(25),DATEPART(DAY, FechaHoraIngreso),DATEPART(MONTH, FechaHoraIngreso)) as DiaEntradaFeriado,
-FechaHoraSalida,
-DATEPART(HOUR, FechaHoraSalida) as HoraSalida, CONVERT(varchar(50), FechaHoraSalida) as EstratoHoraSalida,
-CONVERT(varchar(10), DATEPART(HOUR, FechaHoraSalida)) as IndicativoHoraSalida,
-DATEPART(DAY,FechaHoraSalida) as DiaSalida, DATEPART(WEEKDAY,FechaHoraSalida) as DiaSemanaSalida,
-CONVERT(varchar(50),DATEPART(WEEKDAY,FechaHoraSalida)) as SalidaRestriccion,
-CONVERT(varchar(25),DATEPART(DAY, FechaHoraSalida)) as DiaSalidaFeriado,
-DATEDIFF(MINUTE,FechaHoraIngreso,FechaHoraSalida) as CantMinutos, 
-CONVERT(VARCHAR(50),DATEDIFF(MINUTE,FechaHoraSalida,FechaHoraIngreso)) as EstratoCantMinutos,
-TotalACobrar,
-Ganancia, CONVERT(varchar(50),Ganancia) as EstratoGanancia
-into FactEstacionamiento
-FROM ExamenAnalisis.dbo.Estacionamiento /*[ExamenAnalisis].[dbo].[Vehiculo] v*/
+	TarifaBase, Mantenimiento, ImpVentas, FechaHoraIngreso,
+	DATEPART(HOUR, FechaHoraIngreso) as HoraEntrada, CONVERT(varchar(50), FechaHoraIngreso) as EstratoHoraEntrada,
+	CONVERT(varchar(10), DATEPART(HOUR, FechaHoraIngreso)) as IndicativoHoraEntrada,
+	DATEPART(DAY,FechaHoraIngreso) as DiaEntrada, 
+	DATEPART(WEEKDAY,FechaHoraIngreso) as DiaSemanaEntrada,
+	CONVERT(varchar(50),DATEPART(WEEKDAY,FechaHoraIngreso)) as EntradaRestriccion,
+	CONVERT(varchar(25),DATEPART(DAY, FechaHoraIngreso),DATEPART(MONTH, FechaHoraIngreso)) as DiaEntradaFeriado,
+	FechaHoraSalida,
+	DATEPART(HOUR, FechaHoraSalida) as HoraSalida, CONVERT(varchar(50), FechaHoraSalida) as EstratoHoraSalida,
+	CONVERT(varchar(10), DATEPART(HOUR, FechaHoraSalida)) as IndicativoHoraSalida,
+	DATEPART(DAY,FechaHoraSalida) as DiaSalida, DATEPART(WEEKDAY,FechaHoraSalida) as DiaSemanaSalida,
+	CONVERT(varchar(50),DATEPART(WEEKDAY,FechaHoraSalida)) as SalidaRestriccion,
+	CONVERT(varchar(25),DATEPART(DAY, FechaHoraSalida)) as DiaSalidaFeriado,
+	DATEDIFF(MINUTE,FechaHoraIngreso,FechaHoraSalida) as CantMinutos, 
+	CONVERT(VARCHAR(50),DATEDIFF(MINUTE,FechaHoraSalida,FechaHoraIngreso)) as EstratoCantMinutos,
+	TotalACobrar,
+	Ganancia, CONVERT(varchar(50),Ganancia) as EstratoGanancia
+	into FactEstacionamiento
+	FROM ExamenAnalisis.dbo.Estacionamiento
 
 
 /*indicador entrada*/
@@ -43,38 +43,44 @@ begin tran
                  end
 commit;
 
-/*Indicador Feriado FALTA POCO*/
-/*begin tran
-	update FactEstacionamiento set DiaEntradaFeriado =
-	if (select COUNT(*) FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales] d, [ExamenAnalisis].dbo.Estacionamiento e
-		where d.DiaFeriado = DATEPART(DAY,e.FechaHoraIngreso) AND d.MesFeriado = DATEPART(MONTH, e.FechaHoraIngreso) ) >0
-	select ISNULL(d.NombreFeriado, 'Dia no feriado') FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales] d, [ExamenAnalisis].dbo.Estacionamiento e
-		where d.DiaFeriado = DATEPART(DAY,e.FechaHoraIngreso) AND d.MesFeriado = DATEPART(MONTH, e.FechaHoraIngreso)
-	else
-	select 'Dia no feriado'
-commit;*/
+/*Indicador Dia Entrada Feriado */
+begin tran
+		IF  EXISTS
+		(SELECT d.DiaFeriado, d.MesFeriado, d.NombreFeriado FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales] d, 
+			[ExamenAnalisis].dbo.Estacionamiento e
+			WHERE d.DiaFeriado = DATEPART(DAY,e.FechaHoraIngreso) AND d.MesFeriado = DATEPART(MONTH, e.FechaHoraIngreso))
+			update FactEstacionamiento SET SalidaRestriccion = 'Feriado'
+		SELECT isnull(d.NombreFeriado, '') FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales] d,
+			[ExamenAnalisis].dbo.Estacionamiento e
+			where d.MesFeriado = DATEPART(MONTH,e.FechaHoraIngreso) AND d.DiaFeriado = DATEPART(DAY, e.FechaHoraIngreso)
+			update FactEstacionamiento SET EntradaRestriccion = 'No Feriado'
+commit;
 
-/*BEGIN TRAN 
-UPDATE FactEstacionamiento
-	IF (SELECT count(*) FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales]
-		where [MesFeriado] = DATEPART(MONTH,@fecha) AND [DiaFeriado] = DATEPART(DAY, @fecha) ) >0
-	SELECT isnull([NombreFeriado], 'hola') FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales]
-		where [MesFeriado] = DATEPART(MONTH,@fecha) AND [DiaFeriado] = DATEPART(DAY, @fecha)
-ELSE 
-SELECT 'hello'
-COMMIT;*/
+/*Indicador Dia Salida Feriado */
+begin tran
+		IF  EXISTS
+		(SELECT d.DiaFeriado, d.MesFeriado, d.NombreFeriado FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales] d, 
+			[ExamenAnalisis].dbo.Estacionamiento e
+			WHERE d.DiaFeriado = DATEPART(DAY,e.FechaHoraSalida) AND d.MesFeriado = DATEPART(MONTH, e.FechaHoraSalida))
+			update FactEstacionamiento SET SalidaRestriccion = 'Feriado'
+		SELECT isnull(d.NombreFeriado, '') FROM [ExamenAnalisis].dbo.[DiasFeriadosAnuales] d,
+			[ExamenAnalisis].dbo.Estacionamiento e
+			where d.MesFeriado = DATEPART(MONTH,e.FechaHoraSalida) AND d.DiaFeriado = DATEPART(DAY, e.FechaHoraSalida)
+			update FactEstacionamiento SET SalidaRestriccion = 'No Feriado'
+commit;
 
 /*Indicador Dia Resctriccion Entrada*/
 /*begin tran
-	update FactEstacionamiento
-		set EntradaRestriccion = case
-                  when UltimoDigitoPlaca >=0 AND <=3 AND DiaSemanaEntrada =2  then 'Restriccion'
-                  when UltimoDigitoPlaca >=4 AND <=7 AND DiaSemanaEntrada =3  then 'Restriccion'
-				  when UltimoDigitoPlaca >=8 AND <=1 AND DiaSemanaEntrada =4  then 'Restriccion'
-				  when UltimoDigitoPlaca >=2 AND <=5 AND DiaSemanaEntrada =5  then 'Restriccion'
-				  when UltimoDigitoPlaca >=6 AND <=9 AND DiaSemanaEntrada =6  then 'Restriccion'
-                  else 'NO Restriccion'
-                 end
+	select RIGHT(Placa, 1) AS UltimoDigitoPlaca FROM ExamenAnalisis.dbo.Vehiculo
+		Update FactEstacionamiento set EntradaRestriccion = case 
+					  when UltimoDigitoPlaca >=0 AND UltimoDigitoPlaca <=3 AND DiaSemanaEntrada =2 
+					  then 'Restriccion'
+					  when UltimoDigitoPlaca >=4 AND UltimoDigitoPlaca <=7 AND DiaSemanaEntrada =3  then 'Restriccion'
+					  when UltimoDigitoPlaca >=8 AND UltimoDigitoPlaca <=1 AND DiaSemanaEntrada =4  then 'Restriccion'
+					  when UltimoDigitoPlaca >=2 AND UltimoDigitoPlaca <=5 AND DiaSemanaEntrada =5  then 'Restriccion'
+					  when UltimoDigitoPlaca >=6 AND UltimoDigitoPlaca <=9 AND DiaSemanaEntrada =6  then 'Restriccion'
+		else 'NO Restriccion'
+		end 
 commit;*/
 
 /*Indicador Dia Resctriccion Salida*/
--- qry-table-dim-Vehiculo.sql
+++ qry-table-dim-Vehiculo.sql
@@ -1,14 +1,28 @@
-use CuboParqueo   if
-	(OBJECT_ID ('dbo.DimVehiculo', 'U') > 0) drop table DimVehiculo; 
+use CuboParqueo  
+	if
+	(OBJECT_ID ('dbo.DimVehiculo', 'U') > 0) drop table DimVehiculo; 
 	GO
 
-use ExamenAnalisis  
+use ExamenAnalisis  
 
-	SELECT [ExamenAnalisis].dbo.Vehiculo.IDVehiculo, [ExamenAnalisis].dbo.Vehiculo.Ano, [ExamenAnalisis].dbo.Vehiculo.Placa,
-	[ExamenAnalisis].dbo.TipoVehiculo.Descripcion as NombreTipoVehiculo, [ExamenAnalisis].dbo.Fabricante.NombreFabricante,
+	SELECT [ExamenAnalisis].dbo.Vehiculo.Ano, CONVERT(varchar(50),Ano) as EstratoAnoFabricacion,
+	[ExamenAnalisis].dbo.TipoVehiculo.Descripcion as NombreTipoVehiculo,[ExamenAnalisis].dbo.Fabricante.NombreFabricante
+	into CuboParqueo.dbo.DimVehiculo
+	FROM [ExamenAnalisis].dbo.Vehiculo, [ExamenAnalisis].dbo.TipoVehiculo,  [ExamenAnalisis].dbo.Fabricante
+	
+	
+	/*
 	[ExamenAnalisis].dbo.Pais.NombrePais as PaisFabricante, [ExamenAnalisis].dbo.Pais.AbreviaturaPais as AbrevPaisFabricante,
 	[ExamenAnalisis].dbo.RegionContinente.Descripcion as NombreRegionContinenteFabricante, 
-	[ExamenAnalisis].dbo.Continente.Descripcion as NombreContinenteFabricante
-	into DimVehiculo
-	FROM [ExamenAnalisis].dbo.Vehiculo, [ExamenAnalisis].dbo.TipoVehiculo, [ExamenAnalisis].dbo.Fabricante, [ExamenAnalisis].dbo.Pais,
-	[ExamenAnalisis].dbo.RegionContinente, [ExamenAnalisis].dbo.Continente
+	[ExamenAnalisis].dbo.Continente.Descripcion as NombreContinenteFabricante*/
+	
+	 /*, [ExamenAnalisis].dbo.Fabricante, [ExamenAnalisis].dbo.Pais,
+	[ExamenAnalisis].dbo.RegionContinente, [ExamenAnalisis].dbo.Continente*/
+
+	/*estrato año de fabricacion*/
+	/*begin tran 
+		update DimVehiculo set EstratoAnoFabricacion =
+		( select e.Descripcion from ParqueoEstratos e 
+			where Ano >= e.LimiteInferior and Ano < e.LimiteSuperior 
+			and e.TipoEstrato = 'AnoFabricacion' );   
+	commit;*/
